9ed005bd4fa3b9afc31faa42af74e5a8a4eb9226
Ahmed Sabry <39857006+ahmetsabri@users.noreply.github.com>
2023-07-08 15:37:27 +0300

    version 1

---
+++ src/Sluggable.php
@@ -0,0 +1,14 @@
+<?php
+namespace Ahmed\Abdulhamid;
+use Illuminate\Support\Str;
+
+trait Sluggable {
+
+public static function bootSluggable(){
+
+    static::saving(function(self $model){
+        $slugColumn = $model->slugColumn ?? 'slug';
+        $model->{$slugColumn} = Str::slug($model->{$model->sluggable});
+    });
+}
+}
